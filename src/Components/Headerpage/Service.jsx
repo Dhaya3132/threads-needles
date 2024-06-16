ce7e1bf82597a5e80853eaa740e4e80a2e1d6817
Dhaya3132 <dhayalanu103@gmail.com>
2024-06-16 13:07:44 +0530

    Feat - New Section

--- src/Components/Headerpage/Service.jsx
+++ src/Components/Headerpage/Service.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Service() {
     return (
-        <div className='flex flex-col gap-10 mt-28' >
+        <section className='flex flex-col gap-10 mt-28' >
             <h3 className='font-Sora text-6xl text-center font-semibold'>Service we provide</h3>
             <div className='flex justify-center gap-10 items-center'>
                 <div className="h-32 w-1/6 rounded-full bg-[url('/src/assets/Blouse.jpg')] relative">
@@ -26,7 +26,7 @@ function Service() {
                     <h2 className='font-Sora text-4xl text-black font-semibold text-center top-10 left-10 absolute'>T-Shirts</h2>
                 </div>
             </div>
-        </div>
+        </section>
 
 
     )
