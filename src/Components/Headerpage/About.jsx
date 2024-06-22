786ecc433c4e3b4fa82bec7fd8c62039bb36bdb6
Dhaya3132 <dhayalanu103@gmail.com>
2024-06-22 22:25:22 +0530

    Feat - About Us

--- src/Components/Headerpage/About.jsx
+++ src/Components/Headerpage/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 function About() {
-  return (
-    <section>
-        
-    </section>
-  )
+    return (
+        <section className='flex justify-evenly p-10'>
+            <div>
+                <h3 className='font-Sora font-semibold text-8xl'>About Us</h3>
+                <div className='h-3 bg-gradient-to-r from-Orange to-white'></div>
+            </div>
+            <p className='font-Sora font-extralight text-2xl w-2/5 text-slate-600'>
+                Welcome to thread&needle, where tradition meets modern elegance. Since 2010, we have been dedicated to crafting bespoke garments that perfectly blend timeless style and contemporary flair. Our passion for tailoring is rooted in a deep respect for the art of garment making and a commitment to delivering exceptional quality and personalized service to each of our clients.
+            </p>
+        </section>
+    )
 }
 
 export default About
--- src/Components/Headerpage/Heads.jsx
+++ src/Components/Headerpage/Heads.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar/Navbar';
 import Head from './Header/Head';
 import Service from './Service';
 import Why from './Whyus/Why';
+import About from './About';
 
 
 function Heads() {
@@ -12,6 +13,7 @@ function Heads() {
       <Head></Head>
       <Service></Service>
       <Why></Why>
+      <About></About>
     </div>
   )
 }
